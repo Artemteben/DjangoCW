3f062567f533df9921145593029b59780b94b752
79096 <a.malakhinsky@gmail.com>
2024-11-13 17:05:25 +0300

    26. Создание админки.

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
-# Register your models here.
+from newsletter.models import Client, Mailing, Message, MailingAttempt
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ("email", "fullname", "phone_number", "comment")
+    search_fields = ("email", "fullname", "phone_number")
+
+    list_filter = ("email", "fullname", "phone_number")
+
+
+@admin.register(Mailing)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ("datetime_first_mailing", "frequency", "status")
+    search_fields = ("message",)
+    list_filter = ("frequency", "status", "datetime_first_mailing")
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = (
+        "subject",
+        "content",
+    )
+    search_fields = ("subject",)
+
+
+@admin.register(MailingAttempt)
+class MailingAttemptAdmin(admin.ModelAdmin):
+    list_display = (
+        "datetime_attempt",
+        "status",
+    )
+    list_filter = ("datetime_attempt", "status")
